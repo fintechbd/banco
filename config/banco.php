f21be75dea4d07ca15698b167d63107126591975
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-27 01:49:27 +0600

    LP-1 banco module wrong code removed

--- config/banco.php
+++ config/banco.php
@@ -24,42 +24,6 @@ return [
 
     'root_prefix' => 'test/',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Bank Model
-    |--------------------------------------------------------------------------
-    |
-    | This value will be used to across system where model is needed
-    */
-    'bank_model' => \Fintech\Banco\Models\Bank::class,
-
-    /*
-    |--------------------------------------------------------------------------
-    | BankBranch Model
-    |--------------------------------------------------------------------------
-    |
-    | This value will be used to across system where model is needed
-    */
-    'bank_branch_model' => \Fintech\Banco\Models\BankBranch::class,
-
-    /*
-    |--------------------------------------------------------------------------
-    | BeneficiaryType Model
-    |--------------------------------------------------------------------------
-    |
-    | This value will be used to across system where model is needed
-    */
-    'beneficiary_type_model' => \Fintech\Banco\Models\BeneficiaryType::class,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Beneficiary Model
-    |--------------------------------------------------------------------------
-    |
-    | This value will be used to across system where model is needed
-    */
-    'beneficiary_model' => \Fintech\Banco\Models\Beneficiary::class,
-
     //** Model Config Point Do not Remove **//
 
     /*
@@ -71,14 +35,6 @@ return [
     */
 
     'repositories' => [
-        \Fintech\Banco\Interfaces\BankRepository::class => \Fintech\Banco\Repositories\Eloquent\BankRepository::class,
-
-        \Fintech\Banco\Interfaces\BankBranchRepository::class => \Fintech\Banco\Repositories\Eloquent\BankBranchRepository::class,
-
-        \Fintech\Banco\Interfaces\BeneficiaryTypeRepository::class => \Fintech\Banco\Repositories\Eloquent\BeneficiaryTypeRepository::class,
-
-        \Fintech\Banco\Interfaces\BeneficiaryRepository::class => \Fintech\Banco\Repositories\Eloquent\BeneficiaryRepository::class,
-
         //** Repository Binding Config Point Do not Remove **//
     ],
 
--- routes/api.php
+++ routes/api.php
@@ -15,18 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 if (Config::get('fintech.banco.enabled')) {
     Route::prefix('banco')->group(function () {
-        Route::apiResource('banks', \Fintech\Banco\Http\Controllers\BankController::class);
-        Route::post('banks/bank/restore', [\Fintech\Banco\Http\Controllers\BankController::class, 'restore'])->name('banks.restore');
-
-        Route::apiResource('bank-branches', \Fintech\Banco\Http\Controllers\BankBranchController::class);
-        Route::post('bank-branches/{bank_branch}/restore', [\Fintech\Banco\Http\Controllers\BankBranchController::class, 'restore'])->name('bankbranches.restore');
-
-        Route::apiResource('beneficiary-types', \Fintech\Banco\Http\Controllers\BeneficiaryTypeController::class);
-        Route::post('beneficiary-types/{beneficiary_type}/restore', [\Fintech\Banco\Http\Controllers\BeneficiaryTypeController::class, 'restore'])->name('beneficiarytypes.restore');
-
-        Route::apiResource('beneficiaries', \Fintech\Banco\Http\Controllers\BeneficiaryController::class);
-        Route::post('beneficiaries/beneficiary/restore', [\Fintech\Banco\Http\Controllers\BeneficiaryController::class, 'restore'])->name('beneficiaries.restore');
-
         //DO NOT REMOVE THIS LINE//
     });
 }
