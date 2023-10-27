66b4c5025e130532953634ab67773f87dd6ff457
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-28 01:59:50 +0600

    LP-105 Bank branch fake data store function create

--- tests/Feature/BankBranchTest.php
+++ tests/Feature/BankBranchTest.php
@@ -7,3 +7,16 @@ use MongoDB\Laravel\Eloquent\Model as MONGODB;
 use function Pest\Laravel\getJson;
 use function Pest\Laravel\postJson;
 
+function createBankBranch(): MYSQLDBLEBUPAY|MONGODB|null
+{
+    return \Fintech\Banco\Facades\Banco::bankBranch()->create([
+        'bank_id' => 1,
+        'bank_branch_name' => Str::random(20),
+        'bank_branch_data' => [
+            'routing_number' => 1,
+            'trans_fast_bank_branch_code' => 1,
+            'emq_bank_branch_code' => 1,
+        ],
+        'enabled' => '1',
+    ]);
+}
