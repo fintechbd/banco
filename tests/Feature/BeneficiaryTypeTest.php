e457a232128563c8766d3949a6b86ec9530ef77f
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-28 03:24:25 +0600

    LP-106 beneficiary type test case file create

---
+++ tests/Feature/BeneficiaryTypeTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model as MYSQLDBLEBUPAY;
+use Illuminate\Support\Str;
+use MongoDB\Laravel\Eloquent\Model as MONGODB;
+
+use function Pest\Laravel\deleteJson;
+use function Pest\Laravel\getJson;
+use function Pest\Laravel\postJson;
+use function Pest\Laravel\putJson;
+
