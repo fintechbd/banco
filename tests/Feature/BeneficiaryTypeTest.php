70e93df497535b9d775263b5fcac646952d655cd
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-28 03:50:48 +0600

    LP-106 Beneficiary Type test case multiple faker add

--- tests/Feature/BeneficiaryTypeTest.php
+++ tests/Feature/BeneficiaryTypeTest.php
@@ -14,24 +14,132 @@ use function Pest\Laravel\putJson;
  */
 function createBeneficiaryType(): MYSQLDBLEBUPAY|MONGODB|null
 {
-    return \Fintech\Banco\Facades\Banco::beneficiaryType()->create([
-        'beneficiary_type_name' => Str::random(20),
-        'beneficiary_type_data' => [
-            [
-                "user_recipient_type_condition_name" => "Cash Name",
-                "user_recipient_type_condition_field_name" => "cash_id",
-                "user_recipient_type_condition_field_type" => "select"
+    return \Fintech\Banco\Facades\Banco::beneficiaryType()->create(
+        [
+            'beneficiary_type_name' => "Cash Pickup",
+            'beneficiary_type_data' => [
+                [
+                    "user_recipient_type_condition_name" => "Cash Name",
+                    "user_recipient_type_condition_field_name" => "cash_id",
+                    "user_recipient_type_condition_field_type" => "select"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "Cash Account Number",
+                    "user_recipient_type_condition_field_name" => "cash_account_number",
+                    "user_recipient_type_condition_field_type" => "text"
+                ]
             ],
-            [
-                "user_recipient_type_condition_name" => "Cash Account Number",
-                "user_recipient_type_condition_field_name" => "cash_account_number",
-                "user_recipient_type_condition_field_type" => "text"
-            ]
+            'enabled' => '1',
         ],
-        'enabled' => '1',
-    ]);
+        [
+            'beneficiary_type_name' => "Instant Bank Transfer",
+            'beneficiary_type_data' => [
+                [
+                    "user_recipient_type_condition_name" => "Instant Bank Name",
+                    "user_recipient_type_condition_field_name" => "instant_bank_id",
+                    "user_recipient_type_condition_field_type" => "select"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "Instant Bank Branch Name",
+                    "user_recipient_type_condition_field_name" => "instant_bank_branch_id",
+                    "user_recipient_type_condition_field_type" => "select"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "Instant Bank Account Number",
+                    "user_recipient_type_condition_field_name" => "instant_bank_account_number",
+                    "user_recipient_type_condition_field_type" => "text"
+                ]
+            ],
+            'enabled' => '1',
+        ],
+        [
+            'beneficiary_type_name' => "Bank Transfer",
+            'beneficiary_type_data' => [
+                [
+                    "user_recipient_type_condition_name" => "Bank Name",
+                    "user_recipient_type_condition_field_name" => "bank_id",
+                    "user_recipient_type_condition_field_type" => "select"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "Bank Branch Name",
+                    "user_recipient_type_condition_field_name" => "bank_branch_id",
+                    "user_recipient_type_condition_field_type" => "select"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "Bank Account Number",
+                    "user_recipient_type_condition_field_name" => "bank_account_number",
+                    "user_recipient_type_condition_field_type" => "text"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "ID Number",
+                    "user_recipient_type_condition_field_name" => "id_number",
+                    "user_recipient_type_condition_field_type" => "text"
+                ]
+            ],
+            'enabled' => '1',
+        ],
+        [
+            'beneficiary_type_name' => "Beneficiary",
+            'beneficiary_type_data' => [
+                [
+                    "user_recipient_type_condition_name" => "ID Type",
+                    "user_recipient_type_condition_field_name" => "id_type",
+                    "user_recipient_type_condition_field_type" => "select"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "ID Number",
+                    "user_recipient_type_condition_field_name" => "id_number",
+                    "user_recipient_type_condition_field_type" => "text"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "ID Expire Date",
+                    "user_recipient_type_condition_field_name" => "id_expire_date",
+                    "user_recipient_type_condition_field_type" => "date"
+                ]
+            ],
+            'enabled' => '1',
+        ],
+        [
+            'beneficiary_type_name' => "Wallet Transfer",
+            'beneficiary_type_data' => [
+                [
+                    "user_recipient_type_condition_name" => "Wallet Name",
+                    "user_recipient_type_condition_field_name" => "wallet_id",
+                    "user_recipient_type_condition_field_type" => "select"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "Wallet Account Number",
+                    "user_recipient_type_condition_field_name" => "wallet_account_number",
+                    "user_recipient_type_condition_field_type" => "text"
+                ]
+            ],
+            'enabled' => '1',
+        ],
+        [
+            'beneficiary_type_name' => "B2B Transaction",
+            'beneficiary_type_data' => [
+                [
+                    "user_recipient_type_condition_name" => "Bank Name",
+                    "user_recipient_type_condition_field_name" => "bank_id",
+                    "user_recipient_type_condition_field_type" => "select"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "Bank Branch Name",
+                    "user_recipient_type_condition_field_name" => "bank_branch_id",
+                    "user_recipient_type_condition_field_type" => "select"
+                ],
+                [
+                    "user_recipient_type_condition_name" => "Bank Account Number",
+                    "user_recipient_type_condition_field_name" => "bank_account_number",
+                    "user_recipient_type_condition_field_type" => "text"
+                ]
+            ],
+            'enabled' => '1',
+        ]
+    );
 }
 
 test('Beneficiary Type list', function () {
+    createBeneficiaryType();
     getJson('/api/banco/beneficiary-types')->assertStatus(200);
 });
