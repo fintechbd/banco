5d58dfc1525f8d9941de76091dc249a0ba3eff2d
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-30 01:50:17 +0600

    LP-170 Beneficiary test case faker create

--- tests/Feature/BeneficiaryTest.php
+++ tests/Feature/BeneficiaryTest.php
@@ -10,3 +10,28 @@ use function Pest\Laravel\putJson;
 use function Pest\Laravel\deleteJson;
 use function Pest\Laravel\optionsJson;
 
+/**
+ * @return MYSQLDBLEBUPAY|MONGODB|null
+ */
+function createBeneficiary(): MYSQLDBLEBUPAY|MONGODB|null
+{
+    return \Fintech\Banco\Facades\Banco::bank()->create([
+        'user_id' => 1,
+        'city_id' => 1,
+        'state_id' => 1,
+        'country_id' => 1,
+        'beneficiary_type_id' => 1,
+        'relation_id' => 1,
+        'beneficiary_name' => Str::random(20),
+        'beneficiary_mobile' => '01614747054',
+        'beneficiary_address' => '2 No, Muslim Nagar, Matuail, Tushardhara, Dhaka - 1362',
+        'beneficiary_data' => [
+            "bank_id" => 1,
+            "bank_branch_id" => 1,
+            "bank_account_number" => "123456789",
+            "beneficiary_type" => "individual",
+            "beneficiary_type_condition_name" => "Bank Name"
+        ],
+        'enabled' => '1',
+    ]);
+}
