47e6f87ad083846607e719663759a6932d33c6ff
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-12-13 02:26:24 +0600

    LP-177 php doc update

--- src/Http/Resources/BeneficiaryCollection.php
+++ src/Http/Resources/BeneficiaryCollection.php
@@ -34,6 +34,8 @@ class BeneficiaryCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
+     * @param Request $request
+     * @return array
      */
     public function toArray(Request $request): array
     {
--- src/Http/Resources/BeneficiaryResource.php
+++ src/Http/Resources/BeneficiaryResource.php
@@ -33,6 +33,8 @@ class BeneficiaryResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
+     * @param Request $request
+     * @return array
      */
     public function toArray(Request $request): array
     {
