3893dc44758b4720afe7c85eeb8df5c99f819a8e
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-02-20 22:02:53 +0600

    LP-106 rollback 02

--- src/Interfaces/BeneficiaryTypeRepository.php
+++ src/Interfaces/BeneficiaryTypeRepository.php
@@ -17,46 +17,41 @@ interface BeneficiaryTypeRepository
      * return a list or pagination of items from
      * filtered options
      *
-     * @param array $filters
      * @return Paginator|Collection
      */
-    public function list(array $filters = []): Paginator|Collection;
+    public function list(array $filters = []);
 
     /**
      * Create a new entry resource
      *
-     * @param array $attributes
      * @return EloquentModel|MongodbModel|null
      */
-    public function create(array $attributes = []): EloquentModel|MongodbModel|null;
+    public function create(array $attributes = []);
 
     /**
      * find and update a resource attributes
      *
-     * @param int|string $id
-     * @param array $attributes
      * @return EloquentModel|MongodbModel|null
      */
-    public function update(int|string $id, array $attributes = []): EloquentModel|MongodbModel|null;
+    public function update(int|string $id, array $attributes = []);
 
     /**
-     * find and delete an entry from records
+     * find and delete a entry from records
      *
-     * @param int|string $id
      * @param bool $onlyTrashed
      * @return EloquentModel|MongodbModel|null
      */
-    public function find(int|string $id, bool $onlyTrashed = false): EloquentModel|MongodbModel|null;
+    public function find(int|string $id, $onlyTrashed = false);
 
     /**
-     * find and delete an entry from records
+     * find and delete a entry from records
      */
     public function delete(int|string $id);
 
     /**
-     * find and restore an entry from records
+     * find and restore a entry from records
      *
-     * @throws InvalidArgumentException
+     * @throws \InvalidArgumentException
      */
     public function restore(int|string $id);
 }
