2646b0b3a50fd1e3e3ff06e5b7ea753358b8558f
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-02-20 21:20:31 +0600

    LP-106 Missing function's return type declaration

--- src/Interfaces/BeneficiaryTypeRepository.php
+++ src/Interfaces/BeneficiaryTypeRepository.php
@@ -41,7 +41,7 @@ interface BeneficiaryTypeRepository
      * @param bool $onlyTrashed
      * @return EloquentModel|MongodbModel|null
      */
-    public function find(int|string $id, bool $onlyTrashed = false);
+    public function find(int|string $id, bool $onlyTrashed = false): EloquentModel|MongodbModel|null;
 
     /**
      * find and delete an entry from records
