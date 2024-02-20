cec83d16695f2296f4c07558383f0c829c7f95e5
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-02-20 21:09:35 +0600

    LP-106 PHPDoc comment signature is not complete

--- src/Http/Controllers/BeneficiaryTypeController.php
+++ src/Http/Controllers/BeneficiaryTypeController.php
@@ -159,10 +159,9 @@ class BeneficiaryTypeController extends Controller
      *
      * @lrd:end
      *
+     * @param string|int $id
      * @return JsonResponse
      *
-     * @throws ModelNotFoundException
-     * @throws DeleteOperationException
      */
     public function destroy(string|int $id): JsonResponse
     {
