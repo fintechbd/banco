1abbcf1a6ba471c32bba9ad9a767a11097c6c3bb
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-02-20 21:20:03 +0600

    LP-106 Missing parameter's type declaration

--- src/Interfaces/BeneficiaryTypeRepository.php
+++ src/Interfaces/BeneficiaryTypeRepository.php
@@ -38,10 +38,10 @@ interface BeneficiaryTypeRepository
     /**
      * find and delete a entry from records
      *
-     * @param  bool  $onlyTrashed
+     * @param bool $onlyTrashed
      * @return EloquentModel|MongodbModel|null
      */
-    public function find(int|string $id, $onlyTrashed = false);
+    public function find(int|string $id, bool $onlyTrashed = false);
 
     /**
      * find and delete an entry from records
